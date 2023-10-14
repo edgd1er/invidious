a66fe495488c37e5fe59c281396fe1c293f9cc7d
edgd1er <3601462+edgd1er@users.noreply.github.com>
2023-10-14 19:00:30 +0200

    lint,postgres

--- docker/init-invidious-db.sh
+++ docker/init-invidious-db.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
 set -eou pipefail
 
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/channels.sql
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/videos.sql
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/channel_videos.sql
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/users.sql
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/session_ids.sql
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/nonces.sql
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/annotations.sql
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/playlists.sql
-psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < config/sql/playlist_videos.sql
+# Execute the SQL scripts to create tables and insert initial data
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /config/sql/channels.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /config/sql/videos.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /config/sql/channel_videos.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /config/sql/users.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /config/sql/session_ids.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /config/sql/nonces.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /config/sql/annotations.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /config/sql/playlists.sql
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < /config/sql/playlist_videos.sql
